9de9597e565722a67daf7c5de2e46542782ebc0e
Antti Leppä <antti.leppa@metatavu.fi>
2019-09-19 16:12:18 +0300

    docker

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python3 manage.py migrate --no-input
-python3 manage.py sync_translation_fields --no-input
-python3 manage.py buildstatic --no-input
+python3 manage.py migrate
+python3 manage.py sync_translation_fields
+python3 manage.py buildstatic
 gunicorn --bind 0.0.0.0:8080 -w $WORKER_COUNT linkedevents.wsgi
