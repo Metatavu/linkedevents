89d985be7cd6d71ad46381e8a9fc767ec8ae1a2c
Antti Leppä <antti.leppa@metatavu.fi>
2020-12-17 09:19:52 +0200

    oidc support for docker

--- docker/local_settings.py
+++ docker/local_settings.py
@@ -10,3 +10,34 @@ DATABASES = {
         'PORT': os.environ['DATABASE_PORT'],
     }
 }
+
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 20,
+    'ORDERING_PARAM': 'sort',
+    'DEFAULT_RENDERER_CLASSES': (
+        'events.renderers.JSONRenderer',
+        'events.renderers.JSONLDRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'events.parsers.CamelCaseJSONParser',
+        'events.parsers.JSONLDParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'events.api_pagination.CustomPagination',
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'events.auth.ApiKeyAuthentication',
+        'events.auth.OIDCAuthentication'
+    ),
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
+    'VIEW_NAME_FUNCTION': 'events.api.get_view_name',
+    'OIDC_ENDPOINT': os.environ['OIDC_ENDPOINT'],
+    'OIDC_AUDIENCES': os.environ['OIDC_AUDIENCES'].split(','),
+}
